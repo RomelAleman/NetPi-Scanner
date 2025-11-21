0f00a3ba28132831d2844416f74965d38e33bca4
aidenjajo <ajajo1344@sdsu.edu>
2025-11-20 23:30:14 -0800

    host.py

--- WebUI/host.py
+++ WebUI/host.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import socket
 
 app = Flask(__name__)
 
-# Can add main menu/dashboard here
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# Making another page for scanning, need to create scan.html in templates
-"""
-@app.route('/scan')
+@app.route("/scan")
 def scan():
-    return render_template('scan.html')
-"""
+    return render_template("scan.html")
+
+@app.route("/performance")
+def performance():
+    return render_template("performance.html")
+
+@app.route("/reports")
+def reports():
+    return render_template("reports.html")
+
 
 def get_host_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -27,5 +32,5 @@ def begin_web_ui():
     print(f"Starting Web UI on http://{host_ip}:1234")
     app.run(host=host_ip, port=1234, debug=False, use_reloader=False)
 
-"""if __name__ == '__main__':
-    begin_web_ui()"""
+if __name__ == "__main__":
+    begin_web_ui()
